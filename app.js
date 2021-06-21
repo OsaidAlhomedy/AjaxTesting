0ac961884de9dd7a2d595102d1cd27a535754c07
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-06-21 16:03:53 +0300

    new branch

---
+++ app.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// $('h1').hide();
+// $('.heading2').hide();
+
+// $('#para2 span').css('color','red')
+
+// $('li:nth-child(2n)').css('background-color','red')
+
+// $(':button').hide()
+
+// $('button:first').hover(function(){
+
+//     $('#para1').toggle();
+
+// })
+
+// $('button:last').on('click',function(){
+
+//     $('#para1').show();
+
+// })
+
+
+
+// $('button:first').hover(function(){
+
+//     $('#para1').toggle();
+
+// })
+
+// $(document).on('mousemove',function(e){
+
+//     $('#heading1').html('The Y :' + e.clientY + ' The X :' 
+//     + e.clientX);
+
+// })
+
+// $('#form').on('click',function(e){
+//     e.preventDefault();
+// })
+
+// $('button').on('click',function(e){
+//     let number = e.target.innerHTML;
+//     console.log(number);
+
+//     // $('#input').html('hello')
+// })
+
+
